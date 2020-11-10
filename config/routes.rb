f7977900fd8f2b08add70181aa028c8c64f99342
Tamara Cheikha <tamarasihamcheikha@gmail.com>
2020-11-10 12:25:28 +0000

    index completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # As a user, I can list tasks
-  get 'tasks', to: 'tasks#tasks'
-  # As a user, I can view the details of a task
+  get 'tasks', to: 'tasks#index'
   # As a user, I can add a new task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'restaurant#create'
+  # As a user, I can view the details of a task
+  get 'tasks/:id', to: 'tasks#show'
   # As a user, I can edit a task (mark as completed / update title & details)
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
   # As a user, I can remove a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_113227) do
+ActiveRecord::Schema.define(version: 2020_11_10_113510) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.string "string"
-    t.string "details"
-    t.string "text"
-    t.string "completed"
-    t.string "boolean"
+    t.text "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
