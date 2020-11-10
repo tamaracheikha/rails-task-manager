9596a68e29eac155c2f0ac68a82217be516648b0
Tamara Cheikha <tamarasihamcheikha@gmail.com>
2020-11-10 14:24:27 +0000

    new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,17 +9,28 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
   # As a user, I can add a new task
   get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'restaurant#create'
+  post 'tasks', to: 'tasks#create'
   # As a user, I can view the details of a task
   get 'tasks/:id', to: 'tasks#show', as: :task
   # As a user, I can edit a task (mark as completed / update title & details)
